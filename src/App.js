80697479ae17113427eb6625b2d9b4110dfb68ff
Saosznik <kahockijandrzej@gmail.com>
2022-10-31 10:29:39 +0300

    init: inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+    const handleClick = () => alert('say mio')
+
+
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <div className="app">
+        <input type = 'text' placeholder='login'/>
+        <input type='password' placeholder='password'/>
+      <button onClick={handleClick} >
+          click me
+      </button>
+    </div>
   );
 }
 
